a761db0a6401c25e3ff3e60418fc5a9ccedee17d
ryan.spear <ryan.spear@stud.h-da.de>
2026-09-13 16:32:11 +0200

    chore: build skeleton with miniaudio vendored

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "miniaudio.h"
+
+using std::cout;
+
+int main()
+{
+
+    const char *ma_version_string(void);
+    void ma_version(ma_uint32 * pMajor, ma_uint32 * pMinor, ma_uint32 * pRevision);
+}
